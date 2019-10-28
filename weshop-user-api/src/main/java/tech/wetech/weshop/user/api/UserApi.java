c15d6b691f48202010e988c8812da05eceff9acd
TJR_S <tjr.seeker@gmail.com>
2019-10-28 12:17:01 +0800

    Feign加上fallback

--- weshop-user-api/src/main/java/tech/wetech/weshop/user/api/UserApi.java
+++ weshop-user-api/src/main/java/tech/wetech/weshop/user/api/UserApi.java
@@ -2,11 +2,12 @@ package tech.wetech.weshop.user.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import tech.wetech.weshop.common.api.Api;
+import tech.wetech.weshop.user.fallback.UserApiFallback;
 import tech.wetech.weshop.user.po.User;
 
 /**
  * 额外写一个api出来调用,因为需要写@FeignClient注解，父接口不能代替
  */
-@FeignClient(value = "weshop-user", path = "/shop/user")
+@FeignClient(value = "weshop-user", path = "/shop/user",fallback = UserApiFallback.class)
 public interface UserApi extends Api<User> {
 }
