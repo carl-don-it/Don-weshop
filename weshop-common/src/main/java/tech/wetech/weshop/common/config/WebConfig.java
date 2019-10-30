8dd4106462103a64ad38e8185dcc4106c18de8b6
TJR_S <tjr.seeker@gmail.com>
2019-10-30 20:18:33 +0800

    database,略有完善

--- weshop-common/src/main/java/tech/wetech/weshop/common/config/WebConfig.java
+++ weshop-common/src/main/java/tech/wetech/weshop/common/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     //swagger配置
-    @Override
+    @Override//focus 这些方法有什么用
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
---
+++ weshop-common/src/test/java/test/StringUtilsTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import tech.wetech.weshop.common.utils.StringUtils;
+
+/**
+ * todo
+ *
+ * @author Walker_Don
+ * @version V1.0
+ * @date 2019年10月29日 下午 3:57
+ */
+public class StringUtilsTest {
+    public static void main(String[] args) {
+        String name = "DonWalkeRrJI";
+        System.out.println(StringUtils.camelhumpToUnderline(name));
+    }
+}
