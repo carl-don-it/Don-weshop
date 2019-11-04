7dd2661065336409a65a28eae99b285c07e3e1d3
TJR_S <tjr.seeker@gmail.com>
2019-11-04 15:57:51 +0800

    搭建storage模块

---
+++ weshop-storage/src/main/java/tech/wetech/weshop/storage/WeshopStorageApplication.java
@@ -0,0 +1,25 @@
+package tech.wetech.weshop.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringCloudApplication
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan(value = "tech.wetech.weshop")
+@EnableFeignClients("tech.wetech.weshop.*.api")
+@EnableCaching
+@MapperScan(basePackages = "tech.wetech.weshop.*.mapper")
+public class    WeshopStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeshopStorageApplication.class, args);
+    }
+
+}
---
+++ weshop-storage/src/main/java/tech/wetech/weshop/storage/mapper/StorageMapper.java
@@ -0,0 +1,7 @@
+package tech.wetech.weshop.storage.mapper;
+
+import tech.wetech.weshop.common.utils.MyMapper;
+import tech.wetech.weshop.storage.po.Storage;
+
+public interface StorageMapper extends MyMapper<Storage> {
+}
